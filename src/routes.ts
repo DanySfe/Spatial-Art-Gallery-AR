074feae5413d446a7d96c8ec6269bcdf0637f147
DanySfe <dany@users.noreply.github.com>
2026-02-14 22:35:58 -0500

    Initial commit

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router";
+import Root from "./components/Root";
+import BrandingSystem from "./components/pages/BrandingSystem";
+import UIComponentLibrary from "./components/pages/UIComponentLibrary";
+import AssetLanguage from "./components/pages/AssetLanguage";
+import PerformanceRules from "./components/pages/PerformanceRules";
+import AssetPipeline from "./components/pages/AssetPipeline";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: BrandingSystem },
+      { path: "ui-library", Component: UIComponentLibrary },
+      { path: "asset-language", Component: AssetLanguage },
+      { path: "performance", Component: PerformanceRules },
+      { path: "pipeline", Component: AssetPipeline },
+    ],
+  },
+]);
